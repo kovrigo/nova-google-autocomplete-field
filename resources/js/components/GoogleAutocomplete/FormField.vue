03c1f257928415d408cbcae7968930a3e3045e90
aekovrigo <aekovrigo@gmail.com>
2020-06-24 20:18:59 +0400

    Dev

--- resources/js/components/GoogleAutocomplete/FormField.vue
+++ resources/js/components/GoogleAutocomplete/FormField.vue
@@ -1,86 +1,42 @@
 <template>
-    <default-field :field="this.field">
-        <template slot="field">
-            <div class="form-group">
-                <vue-google-autocomplete
-                        ref="address"
-                        :id="this.field.name"
-                        class="w-full form-control form-input form-input-bordered"
-                        :class="errorClasses"
-                        :placeholder="placeholder"
-                        :country="this.field.countries"
-                        :types="this.field.type"
-                        v-model="value"
-                        v-on:keypress.enter.prevent=""
-                        v-on:placechanged="getAddressData">
-                </vue-google-autocomplete>
-            </div>
+    <div :class="{ invisible: field.invisible }">
+        <default-field :field="field">
+            <template slot="field">
+                <input :id="field.name" type="text"
+                    :disabled="field.disabled"
+                    class="w-full form-control form-input form-input-bordered"
+                    :class="errorClasses"
+                    :placeholder="field.name"
+                    v-model="value"
+                />
 
-            <p v-if="value != ''" class="my-2 text-success">{{ translate.current_address }}: {{ value }}</p>
-
-            <p v-if="hasError" class="help-text error-text mt-2 text-danger">
-                {{ firstError }}
-            </p>
-        </template>
-    </default-field>
+                <p v-if="hasError" class="help-text error-text mt-2 text-danger">
+                    {{ firstError }}
+                </p>
+            </template>
+        </default-field>
+    </div>
 </template>
 
 <script>
 import { FormField, HandlesValidationErrors } from 'laravel-nova'
-import VueGoogleAutocomplete from 'vue-google-autocomplete'
 
 export default {
-    components: { VueGoogleAutocomplete },
-
     mixins: [FormField, HandlesValidationErrors],
 
     props: ['resourceName', 'resourceId', 'field'],
 
-    data: function () {
-        return {
-            address: ''
-        }
-    },
-
-    computed: {
-        translate() {
-            return Nova.config.google_autocomplete_translations
-        },
-
-        placeholder() {
-            if (this.value != '') {
-                return this.translate.update_address
-            }
-
-            return this.field.name
-        }
+    /**
+     * Mount the component.
+     */
+    mounted() {
+        Nova.$on('address-metadata-update-' + this.field.addressValue, ({value}) => {
+            this.value = value;
+        })
     },
 
     methods: {
-
-        /**
-         * Get address
-         */
-        getAddressData: function (addressData, placeResultData) {
-            // Save current data address as a string
-            this.handleChange(placeResultData.formatted_address)
-
-            // Emmit events to by catch up for the other AddressMetadata fields
-            this.field.addressObject.forEach(element => {
-                if(addressData.hasOwnProperty(element)) {
-                    Nova.$emit('address-metadata-update-' + element, {
-                        value: addressData[element]
-                    })
-                }
-
-                if(placeResultData.hasOwnProperty(element)) {
-                    Nova.$emit('address-metadata-update-' + element, {
-                        value: placeResultData[element]
-                    })
-                }
-            });
-        },
-
+        
         /*
          * Set the initial, internal value for the field.
          */
@@ -104,3 +60,10 @@ export default {
     }
 }
 </script>
+
+<style>
+    .invisible {
+        visibility: hidden;
+        position: absolute;
+    }
+</style>
